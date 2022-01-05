c89cff61067c0585f1ac22244a5cc412b080c608
Neitron43 <87646040+Neitron43@users.noreply.github.com>
2022-01-05 15:05:53 +0300

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.header-burger').click(function(event) {
+        $('.header-burger,.header-upper-menu').toggleClass('active');
+    });
+});
+
--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-new Swiper('.swiper-container',{
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    loop: true,
-});
+if ($(window).width() < 1024) {
+    $('.swiper-container').removeClass('_active');
+    slider();
+} else {
+    $('.swiper-container').addClass('_active');
+    slider();
+}   
+   function slider(){
+       new Swiper('._active',{
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        loop: true,
+    });
+   }
